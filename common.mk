743f83564af3524b205047608ae4906384024254
Sri Harsha <srisurya95@users.noreply.github.com>
2015-11-19 21:24:57 -0500

    Add SuperSU Thanks to @Chainfire Change-Id: I1a6e2dd6c962224cb3620e29a89fdd8b3e339e73

--- common.mk
+++ common.mk
@@ -73,6 +73,12 @@ PRODUCT_COPY_FILES +=  \
     vendor/zos/prebuilt/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
     vendor/zos/prebuilt/common/media/PFFprec_600.emd:system/media/PFFprec_600.emd
 
+# SuperSU
+PRODUCT_COPY_FILES += \
+   vendor/zos/prebuilt/common/etc/UPDATE-SuperSU.zip:system/addon.d/UPDATE-SuperSU.zip \
+   vendor/zos/prebuilt/common/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon
+
+
 # Enable SIP+VoIP on all targets
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
