d23f6f0f39720f9197d9b7f3e60a82d29ed3a445
starkdroid <gamerprince.exp@gmail.com>
2016-11-26 01:07:23 +0530

    New Bootanimation!!
    This Dope work deserves a medal,
    Good work @Miradutta

--- common.mk
+++ common.mk
@@ -17,16 +17,46 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Thank you, please drive thru!
 PRODUCT_PROPERTY_OVERRIDES += persist.sys.dun.override=0
 
+#Bootanimation
+
+# Boot animation include
+ifneq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
+
+# determine the smaller dimension
+TARGET_BOOTANIMATION_SIZE := $(shell \
+  if [ $(TARGET_SCREEN_WIDTH) -lt $(TARGET_SCREEN_HEIGHT) ]; then \
+    echo $(TARGET_SCREEN_WIDTH); \
+  else \
+    echo $(TARGET_SCREEN_HEIGHT); \
+  fi )
+
+# get a sorted list of the sizes
+bootanimation_sizes := $(subst .zip,, $(shell ls vendor/zos/prebuilt/common/media))
+bootanimation_sizes := $(shell echo -e $(subst $(space),'\n',$(bootanimation_sizes)) | sort -rn)
+
+# find the appropriate size and set
+define check_and_set_bootanimation
+$(eval TARGET_BOOTANIMATION_NAME := $(shell \
+  if [ -z "$(TARGET_BOOTANIMATION_NAME)" ]; then
+    if [ $(1) -le $(TARGET_BOOTANIMATION_SIZE) ]; then \
+      echo $(1); \
+      exit 0; \
+    fi;
+  fi;
+  echo $(TARGET_BOOTANIMATION_NAME); ))
+endef
+$(foreach size,$(bootanimation_sizes), $(call check_and_set_bootanimation,$(size)))
+
+PRODUCT_COPY_FILES += \
+   vendor/zos/prebuilt/common/media/$(TARGET_BOOTANIMATION_NAME).zip:system/media/bootanimation.zip
+endif
+
 # Backup Tool
 #PRODUCT_COPY_FILES += \
     vendor/zos/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
     vendor/zos/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
     vendor/zos/prebuilt/common/bin/blacklist:system/addon.d/blacklist \
     vendor/zos/prebuilt/common/bin/whitelist:system/addon.d/whitelist \
-
-# Bootanimation
-PRODUCT_COPY_FILES += \
-    vendor/zos/prebuilt/common/media/bootanimation.zip:system/media/bootanimation.zip
 
 # init.d support
 PRODUCT_COPY_FILES += \
