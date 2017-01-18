crdroidandroid/android_vendor_crdroid

vendor: Add maintainers list
Signed-off-by: David Viteri <davidteri91@gmail.com>

config/common.mk
@@ -62,6 +62,10 @@ endif
 PRODUCT_COPY_FILES += \
     vendor/crdroid/prebuilt/common/etc/init.local.rc:root/init.crdroid.rc
 
+# crDroid Maintainers list
+PRODUCT_COPY_FILES += \
+    vendor/crdroid/Maintainers.txt:system/etc/Maintainers.txt
+
 # Copy over added mimetype supported in libcore.net.MimeUtils
 PRODUCT_COPY_FILES += \
     vendor/crdroid/prebuilt/common/lib/content-types.properties:system/lib/content-types.properties

RajGopi7/vendor_zos

Change-Id: Id1889c03479b38bd0f63398d2ab937bffe76369e
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

common.mk
@@ -206,3 +206,7 @@ PRODUCT_BOOT_JARS += \
     org.dirtyunicorns.utils
 PRODUCT_PACKAGES += \
     org.dirtyunicorns.utils
+
+# ZephyrOS Maintainers list
+PRODUCT_COPY_FILES += \
+    vendor/zos/Maintainers.txt:system/etc/Maintainers.txt